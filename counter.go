2844873df1bde0050c9db9b24aa3970efae4067d
ecly <elyne@live.dk>
2017-06-26 15:01:04 +0200

    implemented command line args, added initial test file and correctly scan for first word

--- counter.go
+++ counter.go
@@ -2,9 +2,24 @@ package main
 
 import(
 	"fmt"
+	"os"
+	"bufio"
 )
 
 func main(){
-	fmt.Println("Hello World")
+	args := os.Args[1:]
+	file, err := os.Open(args[0])
+	if err != nil {
+		fmt.Println(err)
+	}
 
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanWords)//set split
+	
+	status := scanner.Scan()
+	if status == false {
+		fmt.Println("Scan failed")
+	}
+
+	fmt.Println("First word: ", scanner.Text())
 }
