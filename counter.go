4e9f969d55f660612a25e68617cdcea7c062fd40
ecly <elyne@live.dk>
2017-06-26 16:06:26 +0200

    finished word count with ipsum test file and optional print amount argument

--- counter.go
+++ counter.go
@@ -4,8 +4,13 @@ import(
 	"fmt"
 	"os"
 	"bufio"
+	"sort"
+	"strconv"
 )
 
+const defaultPrintAmount int = 10
+
+// Input filename and optionally amount of word's occurences to print
 func main(){
 	args := os.Args[1:]
 	file, err := os.Open(args[0])
@@ -15,11 +20,67 @@ func main(){
 
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanWords)//set split
+
+	counter := make(map[string]int)
 	
-	status := scanner.Scan()
-	if status == false {
-		fmt.Println("Scan failed")
+	//While words are available, continue scanning
+	for scanner.Scan() {
+		word := scanner.Text()
+		if _, present := counter[word]; present{
+			counter[word]++
+		} else {
+			counter[word] = 1
+		}
 	}
 
-	fmt.Println("First word: ", scanner.Text())
+
+	//safely parse command line argument
+	//regarding amount of word's occurences to print
+	amountToPrint := defaultPrintAmount
+	if len(args) == 2 {
+		inputAmount, err := strconv.Atoi(args[1])
+		if err == nil {
+			amountToPrint = inputAmount
+		}
+	}
+
+	sortedList := sortMap(counter)
+	prettyPrint(sortedList, amountToPrint)
 }
+
+func sortMap(counter map[string]int) PairList{
+	list := make(PairList, len(counter))
+	i := 0 //index counter
+	for key, val := range counter{
+		list[i] = Pair{key, val}
+		i++
+	}
+
+	//sort it in descending order by value as implemented in interface
+	sort.Sort(sort.Reverse(list))
+	return list
+}
+
+func prettyPrint(list PairList, amountToPrint int) {
+	//Don't allow more than actual word amount
+	if amountToPrint > len(list){
+		amountToPrint = len(list)
+	}
+
+	for i:= 0; i < amountToPrint; i++ {
+		fmt.Printf("%s occured %d times\n", list[i].Key, list[i].Value)
+	}
+}
+
+// Pair used for sorting
+type Pair struct{
+	Key string
+	Value int
+}
+
+// PairList on which we implement the sort interface
+type PairList []Pair
+
+func (p PairList) Len() int           { return len(p) }
+func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p PairList) Less(i, j int) bool { return p[i].Value < p[j].Value }
